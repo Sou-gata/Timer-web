b7aecfb4c92c1f298702c148db336c7e3bfc42f4
Sougata Talukdar <sougatatalukdar77@gmail.com>
2020-12-02 01:43:32 +0530

    add check part

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const TimerSeconds = document.querySelector(".seconds");
 
 ButtonMin.addEventListener("click", () => {
     let InputValue = TimeMin.value;
-    let MintoSec = Math.floor(InputValue) * 60;
+    let MintoSec = Math.floor(CheckValue(InputValue) * 60);
     MainWork(MintoSec);
 });
 ButtonSec.addEventListener("click", () => {
     let InputValue = TimeSec.value;
-    let MintoSec = Math.floor(InputValue);
+    let MintoSec = Math.floor(CheckValue(InputValue));
     MainWork(MintoSec);
 });
 
@@ -40,6 +40,42 @@ function MainWork(MintoSec) {
     }
     let Inturvel = setInterval(CountDown, 1000);
     CountDown();
+}
+function CheckValue(t) {
+    let isnum = /^-?\d+\.?\d*$|^\d*\.?\d+$/.test(t);
+    if (isnum) {
+        if (t >= 0) {
+            return t;
+        } else {
+            setTimeout(() => {
+                TimerMinutes.style.fontSize = "20px";
+                TimerSeconds.style.fontSize = "20px";
+                TimerMinutes.innerHTML = "Negative";
+                TimerSeconds.innerHTML = "Time";
+            }, 1);
+            setTimeout(() => {
+                TimerMinutes.style.fontSize = "50px";
+                TimerSeconds.style.fontSize = "50px";
+                TimerMinutes.innerHTML = `00`;
+                TimerSeconds.innerHTML = `00`;
+            }, 2500);
+            return 0;
+        }
+    } else {
+        setTimeout(() => {
+            TimerMinutes.style.fontSize = "20px";
+            TimerSeconds.style.fontSize = "20px";
+            TimerMinutes.innerHTML = "Invalid";
+            TimerSeconds.innerHTML = "Number";
+        }, 1);
+        setTimeout(() => {
+            TimerMinutes.style.fontSize = "50px";
+            TimerSeconds.style.fontSize = "50px";
+            TimerMinutes.innerHTML = `00`;
+            TimerSeconds.innerHTML = `00`;
+        }, 2500);
+        return 0;
+    }
 }
 function MinSecCalcu(a) {
     let Sec = a % 60;
